98f55b0b7d3d19539e38b898a8b457fc5f970d6e
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-23 01:13:24 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const message = 'Hello world';
+import { initMongoConnection } from "./db/initMongoConnection.js";
+import { Contact } from "./db/models/contacts.js";
+import { setupServer } from "./server.js";
 
-console.log(message);
+(async () => {
+    await initMongoConnection();
+    const contacts = await Contact.find({});
+    console.log(contacts);
+    setupServer();
+})();
