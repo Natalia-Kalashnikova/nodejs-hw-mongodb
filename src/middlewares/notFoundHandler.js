25b15478f1094e1902bd776b631dbd881cb39cdf
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-18 23:51:45 +0300

    Update

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,12 @@
-export const notFoundHandler = (req, res) => {
-  res.status(404).send('Not found!');
+// export const notFoundHandler = (req, res) => {
+//   res.status(404).send('Not found!');
+// };
+
+
+import createHttpError from 'http-errors';
+
+export const notFoundHandler = (req, res, next) => {
+  next(createHttpError(404, 'Not found!'));
 };
 
 
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,3 +1,39 @@
+// import { Router } from "express";
+// import { ctrWrapper } from "../utils/ctrlWrapper.js";
+// import {
+//     loginUserController,
+//     logoutController,
+//     refreshTokenController,
+//     registerUserController
+// } from "../controllers/auth.js";
+// import { registerUserSchema } from "../validation/registerUserSchema.js";
+// import { validateBody } from "../middlewares/validateBody.js";
+// import { loginUserSchema } from "../validation/loginSchemaValidator.js";
+
+// const authRouter = Router();
+
+// authRouter.post(
+//     '/register',
+//     validateBody(registerUserSchema),
+//     ctrWrapper(registerUserController),
+// );
+// authRouter.post(
+//     '/login',
+//     validateBody(loginUserSchema),
+//     ctrWrapper(loginUserController),
+// );
+// authRouter.post(
+//     '/refresh-token',
+//     ctrWrapper(refreshTokenController),
+// );
+// authRouter.post(
+//     '/logout',
+//     ctrWrapper(logoutController),
+// );
+
+
+// export default authRouter;
+
 import { Router } from "express";
 import { ctrWrapper } from "../utils/ctrlWrapper.js";
 import {
@@ -23,7 +59,7 @@ authRouter.post(
     ctrWrapper(loginUserController),
 );
 authRouter.post(
-    '/refresh-token',
+    '/refresh',
     ctrWrapper(refreshTokenController),
 );
 authRouter.post(
